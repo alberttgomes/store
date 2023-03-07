ff7a04a330a418c89adf15644e920364068361ac
Albert Gomes <albert.gomes@liferay.com>
2023-03-07 20:09:25 -0300

    SF

--- src/pages/admin/AdminScreen.tsx
+++ src/pages/admin/AdminScreen.tsx
@@ -1,10 +1,15 @@
-import React, {FC, useState} from "react";
+import React, {FC, useState, useContext} from "react";
 import axios from "axios";
 import "./AdminStyles.scss";
+import {AdminContext} from "../../context/AdminContext";
+import {PublicationContext} from "./../../context/AddPublication";
 
 interface IPropsAdminScreen {}
 
 const AdminScreen: FC<IPropsAdminScreen> = () => {
+
+    const {props, setProps} = useContext(AdminContext);
+    const {propsPublication, setPropsPublication} = useContext(PublicationContext);
     
     const OnSubmit = () => {
         const [response, setResponse] = useState<any>();
@@ -13,13 +18,33 @@ const AdminScreen: FC<IPropsAdminScreen> = () => {
 
             const response = await axios.get(url, payload);
 
+            response !== null || undefined? 
+                setProps(response.data) : undefined
+                
             setResponse(response as any);
         }
 
         return response;
     }
+
+    const AddPublication = () => {
+
+        React.useEffect(() => {
+            async(url: string, payload: Object ) => {
+
+                const response = await axios.post(url, payload);
+
+                if (response.data !== null) {
+                    setPropsPublication(response.data);
+                }
+                else {
+                    console.log(response)
+                }     
+            }
+        }, []);
+    }
     
-    return(
+    return props?.user.name === '' ? (
         <div className="${prefix}-main-screen-admin">
             <a href="/log_in_admin">
                 <h2 className="${prefix}-h2-login-title">Log in</h2>
@@ -36,6 +61,23 @@ const AdminScreen: FC<IPropsAdminScreen> = () => {
                     Acessar
                 </button>
             </div>
+        </div>
+    ) : (
+        <div className="${prefix}-main-screen-admin">
+                <h2>Add Publication</h2>
+                <div className="${prefix}-wrapper-product-items">
+                    <form>
+                        <input>{propsPublication?.product.href}</input>
+                        <input>{propsPublication?.product.image}</input>
+                    </form>
+                </div>
+                <div className="${prefix}-wrapper-button-add">
+                    <button
+                        onClick={AddPublication}
+                    >
+                        Adicionar
+                    </button>
+                </div>
         </div>
     );
 };
